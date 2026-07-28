9ddc04ffbb088cc49887ad419ba224d372864c84
bebop-cow <tyrinfern@yahoo.com>
2026-07-28 13:40:24 -0400

    ais stream

--- sitrisk.py
+++ sitrisk.py
@@ -1,3 +1,33 @@
+import asyncio
+import json
+import websockets
+
+async def stream_ais(api_key, seconds=60):
+    url = "wss://stream.aisstream.io/v0/stream"
+    # bounding boxes: AISStream wants [[[lat,lon],[lat,lon]]] per box
+    subscribe = {
+        "APIKey": 215f573214f83c1150f0e87459cea4c0700e4532,
+        "BoundingBoxes": [
+            [[24.0, 54.0], [27.5, 58.5]],   # Hormuz
+            [[11.5, 42.0], [14.5, 44.5]],   # Bab-el-Mandeb
+        ],
+        "FilterMessageTypes": ["PositionReport"],
+    }
+
+    async with websockets.connect(url) as ws:
+        await ws.send(json.dumps(subscribe))   # send subscription
+        print("connected, streaming...")
+
+        end = asyncio.get_event_loop().time() + seconds
+        async for message in ws:               # messages stream in
+            data = json.loads(message)
+            print(data.get("MessageType"), "-", 
+                  data.get("MetaData", {}).get("ShipName", "?"))
+            if asyncio.get_event_loop().time() > end:
+                break
+
+asyncio.run(stream_ais("PASTE_YOUR_KEY", 30))
+
 
 
 ZONES = {
