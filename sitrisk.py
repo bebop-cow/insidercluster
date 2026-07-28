438deccce0f1943820c8f157ad61d754bba75633
bebop-cow <tyrinfern@yahoo.com>
2026-07-27 23:19:37 -0400

    sitrisk inzone, ship class and tanker relevancy functions

--- sitrisk.py
+++ sitrisk.py
@@ -1,3 +1,5 @@
+
+
 ZONES = {
     "Hormuz":      {"lat": (24.0, 27.5), "lon": (54.0, 58.5)},
     "BabElMandeb": {"lat": (11.5, 14.5), "lon": (42.0, 44.5)},
@@ -7,3 +9,14 @@ def in_zone(lat, lon, zone):
     lat_min, lat_max = zone["lat"]
     lon_min, lon_max = zone["lon"]
     return lat_min <= lat <= lat_max and lon_min <= lon <= lon_max
+
+def classify_ship(msg):
+    shiptype = msg.get("ShipType", 0)
+    return 80 <= shiptype <= 89
+
+def is_relevant_tanker(msg, zone):
+    lat = msg.get("Latitude",999)
+    lon = msg.get("Longitude", 999)
+
+    relevant = in_zone(lat, lon, zone) and classify_ship(msg)
+    return relevant
