f01e174205e2b520453a0301847a1652e917732c
bebop-cow <tyrinfern@yahoo.com>
2026-08-23 20:23:48 -0700

    insider cluster v6, annual_vol update part 2

--- annual_vol.py
+++ annual_vol.py
@@ -1,9 +1,22 @@
 import numpy as np
 import yfinance as yf
+import pandas as pd
+
+def flatten_columns(df):
+	if isinstance(df.columns, pd.MultiIndex):
+		df.columns = df.columns.get_level_values(0)
+	return df
 
 def annual_vol(ticker, years = 1):
-	tk = yf.Ticker(ticker)
-	last_close = tk.history(period="5d")["Close"].iloc[-1]
-	ret = df["Close"].pct_change() * 100
-	return ret.dropna()
+	end = pd.Timestamp.now()
+	start = end - pd.DateOffset(years=years)
+	df = yf.download(ticker, start=start.strftime("%Y-%m-%d"),
+		end=end.strftime("%Y-%m-%d"), progress=False)
+	if df.empty:
+		return None
+	df = flatten_columns(df)
+	ret = df["Close"].pct_change().dropna() * 100
+	av = ret.std() * np.sqrt(252) 
+	return av
+
 
